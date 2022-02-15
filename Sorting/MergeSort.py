8c3231660bbba3f3a408a390d9e6da6254250c46
SDido <bookidodo@gmail.com>
2022-02-15 12:51:57 +0200

    implemented MergeSort

--- Sorting/MergeSort.py
+++ Sorting/MergeSort.py
@@ -1,7 +1,49 @@
+######################################################################
+# Merge Sort 
+# Note: Lists as well as Dicts and Sets are PASSED BY REFERENCE in python
+######################################################################
+
 def merge_sort(lst):
     if len(lst) > 1:
         mid = len(lst)//2
-        left = merge_sort(lst[:mid])
-        right = merge_sort(lst[mid:])
+        left = lst[mid:]
+        right = lst[:mid]
+        merge_sort(left)
+        merge_sort(right)
 
+        i = 0
+        j = 0
+        k = 0
         
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst[k] = left[i]
+            k += 1
+            i += 1
+
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+        
+import timeit
+
+lst = [5,4,2,1,5,7,5,3,4,1,6,8,5]
+
+start = timeit.timeit('output = 10*3')
+
+merge_sort(lst)
+
+end = timeit.timeit('output = 10*3')
+
+print(lst)
+print(f"was done in {str(end - start)[:6]}ms")
+
---
+++ Sorting/README.md
@@ -0,0 +1,50 @@
+# Algorithms in Python
+
+## <ins>Comparison Sorts</ins>
+
+### ⬚ InsertionSort 
+### ⬚ Heapsort
+### ⬚ Quicksort
+### ⬚ MergeSort
+### ⬚ In-placeMergeSort
+
+### ⬚ Introsort
+### ⬚ BlockSort
+### ⬚ TimSort
+### ⬚ SelectionSort
+### ⬚ CubeSort
+### ⬚ ShellSort
+### ⬚ BubbleSort
+### ⬚ ExchangeSort  
+### ⬚ TreeSort
+### ⬚ CycleSort
+### ⬚ LibrarySort
+### ⬚ PatienceSorting
+### ⬚ SmoothSort
+### ⬚ StrandSort
+### ⬚ TournamentSort
+### ⬚ CocktailShakerSort
+### ⬚ CombSort
+### ⬚ GnomeSort
+### ⬚ OddEvenSort
+
+
+## <ins>Non Comparison Sorts</ins>
+
+### ⬚ PigeonholeSort
+### ⬚ BucketSort (uniform keys) 
+### ⬚ BucketSort (integer keys) 
+### ⬚ CountingSort
+### ⬚ LSDRadixSort
+### ⬚ MSDRadixSort
+### ⬚ MSDRadixSort (in-place) 
+### ⬚ SpreadSort
+### ⬚ BurstSort
+### ⬚ FlashSort
+### ⬚ PostmanSort
+
+## <ins>Graphs</ins>
+
+### ⬚ Dijkstra
+
+### References: https://en.wikipedia.org/wiki/Sorting_algorithm
